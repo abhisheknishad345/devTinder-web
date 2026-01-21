8b124a3b7fd2ade644067157dfb2d04adc13a5fa
Abhishek Nishad <abhishekk77625@gmail.com>
2026-01-21 06:25:41 +0530

    login pg

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,9 +1,40 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   return (
-    <div>Login Page</div>
+    <div className='flex justify-center my-10'>
+      <div className="card bg-base-300 w-96 shadow-sm ">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <p>Email ID: {email}</p>
+          <label className="floating-label">
+            <input 
+            type="text" 
+            value={email}
+             className="input input-md" 
+             onChange={(e) => setEmail(e.target.value)}
+             />
+             
+          </label>
+          <p>Password: {password}</p>
+          <label className="floating-label">
+            <input type="text"
+            value={password} 
+             className="input input-md"
+             onChange={(e) => setPassword(e.target.value)}
+              />
+
+          </label>
+
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
