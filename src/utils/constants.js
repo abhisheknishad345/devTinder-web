15a3d75e8fa5aa89cae014c97954808fac6f1959
Abhishek Nishad <abhishekk77625@gmail.com>
2026-08-10 15:40:21 +0530

    changed Base_url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,6 @@
 
-export const BASE_URL = "http://localhost:5700"
+// export const BASE_URL = "http://localhost:5700"
+export const BASE_URL = "https://devtinder-rsj5.onrender.com"
 
 export const profileUrl = "https://plus.unsplash.com/premium_photo-1689977927774-401b12d137d6?w=600&auto=format&fit=crop&q=60"
  
